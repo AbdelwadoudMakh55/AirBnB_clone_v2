791d92177282905c456674d2e47566bb57506da0
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-12-19 12:47:30 +0000

    Temporary

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
 
 
 class Amenity(BaseModel):
-    name = ""
+    """ This is the amenity class """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = 
--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    @property
+    def cities(self):
+        from models import storage
+        obj_list = storage.all()
+        cities = []
+        for obj in obj_list:
+            if type(obj) is City:
+                if obj.state_id == self.id:
+                    cities.append(obj)
+        return cities
