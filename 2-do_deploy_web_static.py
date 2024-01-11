8de93cad5fccc26c0a002107bc984ba4bd386a32
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-11 22:12:57 +0000

    First deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ This is the 2-do_deploy_web_static.py module.
 This module distribute the static content (html, css, images) to the servers
 """
 
-from fabric.api import put, run, env, task
+from fabric.api import put, run, env
 import os
 env.hosts = ['52.91.182.154', '34.202.164.102']
 env.user = 'ubuntu'
@@ -16,16 +16,15 @@ def do_deploy(archive_path):
     if os.path.exists(archive_path) is False:
         return False
     try:
-        path = archive_path.split('/')
-        file_name = path[1]
+        file_name = archive_path.split('/')[1]
         no_ext = file_name.split('.')[0]
         put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/" + no_ext + "/")
         run("tar -xzf /tmp/" + file_name + " -C /data/web_static/releases/"
             + no_ext + "/")
+        run("rm -rf /tmp/" + file_name)
         run("mv /data/web_static/releases/" + no_ext + "/web_static/*"
             + " /data/web_static/releases/" + no_ext + "/")
-        run("rm -rf /tmp/" + file_name)
         run("rm -rf /data/web_static/releases/" + no_ext + "/web_static")
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/" + no_ext + "/ "
