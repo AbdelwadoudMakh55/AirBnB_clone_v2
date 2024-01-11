34f645d5c21fce36ffc2cee05c2217edf378c5bd
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-11 19:29:34 +0000

    First deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-This is the 2-do_deploy_web_static.py module.
-This module distribute the static content (html, css, images) to the servers
-"""
+"""This is the 2-do_deploy_web_static.py module.
+This module distribute the static content (html, css, images) to the servers"""
 
 
 from fabric.api import put, run, env, task
 import os
-
-
 env.hosts = ['52.91.182.154', '34.202.164.102']
 env.user = 'ubuntu'
 env.key_filename = "~/.ssh/id_rsa"
@@ -17,8 +13,6 @@ env.key_filename = "~/.ssh/id_rsa"
 @task
 def do_deploy(archive_path):
     """This is the function for deploying the static content"""
-    import os
-    """ This is the function for deploying the static content """
     if os.path.exists(archive_path) is False:
         return False
     try:
