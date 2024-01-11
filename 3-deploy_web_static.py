9c8f6d2ce75f5dcfba82ee1465fe3df5c1d64957
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-11 23:54:50 +0000

    Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -32,23 +32,23 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """This is the function for deploying the static content"""
+    """ This is the function for deploying the content """
     if os.path.exists(archive_path) is False:
         return False
     try:
-        path = archive_path.split('/')
-        file_name = path[1]
+        p = "/data/web_static/releases/"
+        file_name = archive_path.split('/')[1]
         no_ext = file_name.split('.')[0]
+        sym_link = "/data/web_static/current"
         put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/" + no_ext + "/")
-        run("tar -xzf /tmp/" + file_name + " -C /data/web_static/releases/"
-            + no_ext + "/")
-        run("mv /data/web_static/releases/" + no_ext + "/web_static/*"
-            + " /data/web_static/releases/" + no_ext + "/")
+        run("tar -xzf /tmp/" + file_name + " -C " + p + no_ext + "/")
         run("rm -rf /tmp/" + file_name)
-        run("rm -rf /data/web_static/current")
-        run("ln -sf /data/web_static/releases/" + no_ext + "/ "
-            + "/data/web_static/current")
+        run("mv " + p + no_ext + "/web_static/* " + p + no_ext + "/")
+        run("rm -rf /tmp/" + file_name)
+        run("rm -rf /data/web_static/releases/" + no_ext + "/web_static")
+        run("rm -rf " + sym_link)
+        run("ln -s /data/web_static/releases/" + no_ext + "/ " + sym_link)
         return True
     except Exception:
         return False
