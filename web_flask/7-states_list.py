825176889514799d2bc9130ee6f01e6cb2d3b840
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-28 21:03:44 +0000

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,17 +14,17 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """ Closes the storage """
+    storage.close()
+
+
 @app.route("/states_list")
 def states_list():
     """ Return the list of states """
     states_A_Z = sorted(list(states.values()), key=lambda att: att.name)
     return render_template("7-states_list.html", states=states_A_Z)
-
-
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    """ Closes the storage """
-    storage.close()
 
 
 if __name__ == '__main__':
