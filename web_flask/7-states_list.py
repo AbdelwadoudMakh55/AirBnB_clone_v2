a9d67d7308037ae27361487c8cc8265cc18893e3
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-28 12:06:01 +0000

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is the number_template module
+"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+states = storage.all(State)
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/states_list")
+def states_list():
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
