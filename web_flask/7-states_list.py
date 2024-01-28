bb0d0db5eae7e1bf591b1a41caa875ac5cae493b
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-28 13:06:14 +0000

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -19,5 +19,10 @@ def states_list():
     return render_template("7-states_list.html", states=states)
 
 
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
