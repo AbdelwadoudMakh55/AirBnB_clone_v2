41bb5316dd53748d333981951fcace82a29f0286
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-28 21:08:54 +0000

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,7 +9,6 @@ from models import storage
 from models.state import State
 
 
-states = storage.all(State)
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -23,6 +22,7 @@ def teardown_appcontext(exception):
 @app.route("/states_list")
 def states_list():
     """ Return the list of states """
+    states = storage.all(State)
     states_A_Z = sorted(list(states.values()), key=lambda att: att.name)
     return render_template("7-states_list.html", states=states_A_Z)
 
