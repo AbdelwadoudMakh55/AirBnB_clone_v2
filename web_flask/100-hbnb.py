cf3d428fee11459e7869183ed942b0b90ac36009
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-29 23:59:29 +0000

    Last changes on jinja template

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -8,6 +8,7 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
 
 
 app = Flask(__name__)
@@ -26,8 +27,10 @@ def hbnb_filters():
     states_srtd = sorted(states, key=lambda obj: obj.name)
     amnt = storage.all(Amenity).values()
     amnt_srtd = sorted(amnt, key=lambda obj: obj.name)
+    plc = storage.all(Place).values()
+    plc_srtd = sorted(plc, key=lambda obj: obj.name)
     return render_template("100-hbnb.html", amnt=amnt_srtd,
-                           states=states_srtd)
+                           states=states_srtd, places=plc_srtd)
 
 
 if __name__ == '__main__':
